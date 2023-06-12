8147da5f17664c4026cc7c86617d892d653b9720
Mai Cabarrubias <shelley.cabarrubias@pantheon.io>
2023-06-12 19:10:08 +0800

    DB creation

--- dbsorya/dbsorya/settings.py
+++ dbsorya/dbsorya/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'dbsorya.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbsorya_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
