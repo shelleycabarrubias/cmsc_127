9595690d042e6ef30f8e1d653b514847c94930e5
Mai Cabarrubias <shelley.cabarrubias@pantheon.io>
2023-06-12 19:30:13 +0800

    Adding views.py, forms.py, urls.py, and models.py

--- dbsorya/dbsorya/models.py
+++ dbsorya/dbsorya/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    CATEGORY_CHOICES = [
+        ('food_item', 'Food Item'),
+        ('non_food_item', 'Non-Food Item'),
+    ]
+    name = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
 
 class Supplier(models.Model):
     name = models.CharField(max_length=100)
@@ -9,20 +13,12 @@ class Supplier(models.Model):
     contact_number = models.CharField(max_length=20)
 
 class Product(models.Model):
-    product_code = models.CharField(max_length=20, unique=True)
+    product_code = models.CharField(max_length=20)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     date_acquired = models.DateField()
     expiration_date = models.DateField()
-    quantity = models.PositiveIntegerField()
+    quantity = models.IntegerField()
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     storage_location = models.CharField(max_length=100)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-
-    class Meta:
-        constraints = [
-            models.CheckConstraint(
-                name='check_category',
-                check=models.Q(category__name='food_item') | models.Q(category__name='non_food_item')
-            )
-        ]
